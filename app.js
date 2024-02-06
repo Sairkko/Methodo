974a919a8cf8dddec6a398f527ae8b8c28a8c08f
jeremy <jeremyrossi1234@gmail.com>
2024-02-06 12:54:15 +0100

    Feat(map-google): Api front

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ function initMap() {
         map: map
     });
 }
+
+
+function searchParking() {
+    var searchText = document.getElementById('searchInput').value;
+    var apiUrl = `URL_DE_LAPI?recherche=${encodeURIComponent(searchText)}`;
+
+    axios.get(apiUrl)
+        .then(function(response) {
+            // Reponse doit etre de la forme :
+            // 0 {
+            //      id:
+            //      lat :
+            //      long:
+            // }
+            response.data.forEach(parking => {
+                var position = {lat: parking.lat, lng: parking.long};
+                new google.maps.Marker({
+                    position: position,
+                    map: window.map,
+                    title: `Parking ID: ${parking.id}`
+                });
+            });
+        })
+        .catch(function(error) {
+            console.log('Erreur lors de la récupération des données:', error);
+        });
+}
